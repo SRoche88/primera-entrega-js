e7ffb157e324eb3753f34cb22c76e42ccce514d8
SRoche88 <119961805+SRoche88@users.noreply.github.com>
2022-12-06 17:26:28 -0300

    Final

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-function Valores() {
-    
-    let destino = prompt ("ingresa tu destino");
-    
-    let presupuesto = prompt ("ingresa tu presupuesto");
-    
-    let transporte = prompt("ingresa tus gastos de transporte");
-    
-    let alojamiento = prompt("ingresa tus gastos de alojamiento");
-    
-    let comida = prompt("ingresa tus gastos de comida");
-
-}
-
 function calgastos() {
-   
-  Valores()
- 
-    let gastos = parseInt(transporte) + parseInt(alojamiento) + parseInt(comida);
 
-    let balance = presupuesto - gastos ;
+  // ingreso de valores//
 
-    alert(balance);
+let destino = prompt ("ingresa tu destino");
 
-}
+let presupuesto = prompt ("ingresa tu presupuesto");
 
-function balancefinal() {
+let transporte = prompt("ingresa tus gastos de transporte");
+
+let alojamiento = prompt("ingresa tus gastos de alojamiento");
+
+let comida = prompt("ingresa tus gastos de comida");
+
+ const valores = [destino, presupuesto, transporte, alojamiento, comida];
+
+ console.log(valores);
+
+   // calculo de gastos //
+
+ let gastos = parseInt(transporte) + parseInt(alojamiento) + parseInt(comida);
+
+ console.log(gastos);
+  
+       // balance de gastos contra el presupuesto //
+
+ let balance = presupuesto - gastos ;
+
+ console.log(balance)
+
+   if(balance < 0) {
+
+     alert ("tu presupuesto no es suficiente para viajar a " + destino + ", te faltan $"+balance + " a seguir ahorrando");
+     
+     }
+     
+     else if(balance > 0) {
+     
+     alert("Felicitaciones podes relizar tu viaje a " + destino + ", tenes un exedente de $" + balance + " que disfrutes tus vacaciones");
     
-      if(balance < 0) {
-        alert ("tu presupuesto no es suficiente para viajar a " + destino + ", te faltan $" +balance + "a seguir ahorrando");
-      }
-      else if(balance > 0) {
-       
-        alert("Felicitaciones podes relizar tu viaje a" + destino + ", tenes un exedente de $" + balance + "puedes pasar por el giftshop");
+     
 
-    }  
-  }
-    calgastos()
-    balancefinal()
+ }  
+ }
+
+calgastos()   
 
