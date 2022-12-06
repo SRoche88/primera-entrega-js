a2be97b1038b78d6b43c04e0f75f9998b0c13937
SRoche88 <119961805+SRoche88@users.noreply.github.com>
2022-12-06 14:32:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function Valores() {
+    
+    let destino = prompt ("ingresa tu destino")
+    
+    let presupuesto = prompt ("ingresa tu presupuesto")
+    
+    let transporte = prompt("ingresa tus gastos de transporte")
+    
+    let alojamiento = prompt("ingresa tus gastos de alojamiento")
+    
+    let comida = prompt("ingresa tus gastos de comida")
+
+}
+ 
+function calgastos (){
+   
+    const { destino, presupuesto, transporte, alojamiento, comida} = Valores()
+ 
+    let gastos = parseInt(transporte) + parseInt(alojamiento) + parseInt(comida)
+
+    let balance = presupuesto - gastos
+
+    alert(balance)
+
+}
+
+function balancefinal() {
+
+    const {balance} = getValues()
+  
+  
+      if(balance < 0) {
+        alert ("tu presupuesto no es suficiente para esta viaje, te faltan $" +balance + "a seguir ahorrando")
+      }
+      else if(balance > 0) {
+       
+        alert("Felicitaciones podes relizar tu viaje, tenes un exedente de $" + balance + "puedes pasar por el giftshop")
+
+    }    
+      
+  }
+
+
