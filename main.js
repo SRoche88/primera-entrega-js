b7d35913c6b91462bcec1007c3e7230c3f5eb804
SRoche88 <119961805+SRoche88@users.noreply.github.com>
2022-12-06 16:26:10 -0300

    sin const

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ function Valores() {
 
 function calgastos() {
    
-    const { destino, presupuesto, transporte, alojamiento, comida} = Valores();
  
     let gastos = parseInt(transporte) + parseInt(alojamiento) + parseInt(comida);
 
@@ -25,10 +24,7 @@ function calgastos() {
 }
 
 function balancefinal() {
-
-    const {balance} = calgastos();
-  
-  
+    
       if(balance < 0) {
         alert ("tu presupuesto no es suficiente para viajar a " + destino + ", te faltan $" +balance + "a seguir ahorrando");
       }
